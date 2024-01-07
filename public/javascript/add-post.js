71abbb195f3af40c68ee9f54bfda8e0e07774e0a
mathilde-01 <mathilde.7892@gmail.com>
2024-01-07 15:43:19 -0500

    added javascript file and fixed paths

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,25 +1,28 @@
+// function to add a post
 async function newFormHandler(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const title = document.querySelector('input[name="post-title"]').value;
-    const post_text = document.querySelector('textarea[name="post-text"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const post_text = document.querySelector('textarea[name="post-text"]').value;
 
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        post_text
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+  const response = await fetch(`/api/posts`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      post_text,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
-    }
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+}
+
+document
+  .querySelector(".new-post-form")
+  .addEventListener("submit", newFormHandler);
